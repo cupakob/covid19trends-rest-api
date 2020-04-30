dbd5a0713d2fb197d8c12677e35f6282fb7f3871
Atanas Alexandrov <sirakov@gmail.com>
2020-04-30 06:30:09 +0200

    Add countrycode validator

---
+++ domain/countrycode_test.go
@@ -0,0 +1,31 @@
+package domain_test
+
+import (
+	"github.com/cupakob/covid19trends-rest-api/domain"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCountryCode_Validate(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+	    // given
+	    countryCode := domain.CountryCode("DE")
+
+	    // when
+		validationResult := countryCode.Validate()
+
+		// then
+	    assert.True(t, validationResult)
+	})
+
+	t.Run("validation result is false", func(t *testing.T) {
+		// given
+		countryCode := domain.CountryCode("de")
+
+		// when
+		validationResult := countryCode.Validate()
+
+		// then
+		assert.False(t, validationResult)
+	})
+}
