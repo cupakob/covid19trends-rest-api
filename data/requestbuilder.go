573ab020a27b939347bb404f609ccd59ffea39fa
Atanas Alexandrov <sirakov@gmail.com>
2022-12-07 21:06:43 +0100

    NO-TICKET Fix some linter issues

--- data/requestbuilder.go
+++ data/requestbuilder.go
@@ -2,15 +2,18 @@ package data
 
 import "net/http"
 
+// Builder is interface with a method to build the request
 type Builder interface {
 	BuildRequest() (*http.Request, error)
 }
 
+// RequestBuilder implements Builder
 type RequestBuilder struct {
 	Method string
 	URL    string
 }
 
+// NewRequestBuilder returns an instance of the request builder
 func NewRequestBuilder(method string, url string) Builder {
 	return &RequestBuilder{
 		Method: method,
@@ -18,6 +21,7 @@ func NewRequestBuilder(method string, url string) Builder {
 	}
 }
 
+// BuildRequest builds the request
 func (r RequestBuilder) BuildRequest() (*http.Request, error) {
 	var request *http.Request
 	var err error
